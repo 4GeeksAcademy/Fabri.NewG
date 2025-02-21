dc39b7583cb1ab8c3461e2f50461c9620839a7cd
Fabrigasman <fabriciogasaro@gmail.com>
2025-02-21 12:06:30 +0000

    newF

---
+++ app.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    function generateExcuse() {
+        let who = ["El perro", "Mi madre", "El vecino", "Mi amigo"];
+        let action = ["Comió", "Rompió", "Robó", "Dañó"];
+        let what = ["mi tarea", "mi teléfono", "mi coche"];
+        let when = [
+            "antes de la clase",
+            "cuando estaba durmiendo",
+            "mientras hacía ejercicio",
+            "durante mi almuerzo",
+            "mientras estaba rezando"
+        ];
+        let excuse = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`;
+        document.getElementById("textoExcusa").innerText = excuse;
+    }
+    generateExcuse();
+  };
